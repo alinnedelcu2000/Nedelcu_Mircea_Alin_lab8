0fd4fb60f9f72201697053259275951f68ddab24
Nedelcu Alin <alinnedelcu46@yahoo.com>
2021-11-28 16:58:26 +0200

    Add project files.

---
+++ Data/Nedelcu_Mircea_Alin_lab8Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Nedelcu_Mircea_Alin_lab8.Models;
+
+namespace Nedelcu_Mircea_Alin_lab8.Data
+{
+    public class Nedelcu_Mircea_Alin_lab8Context : DbContext
+    {
+        public Nedelcu_Mircea_Alin_lab8Context (DbContextOptions<Nedelcu_Mircea_Alin_lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Nedelcu_Mircea_Alin_lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Nedelcu_Mircea_Alin_lab8.Models.Publisher> Publisher { get; set; }
+    }
+}
